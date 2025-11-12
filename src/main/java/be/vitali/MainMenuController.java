9758316d2b6cfb5f0eaa527004f1a35ae573fb30
vitalibeniamino <benvit@outlook.com>
2025-11-12 21:50:11 +0100

    moved creation of elements to java code

--- src/main/java/be/vitali/MainMenuController.java
+++ src/main/java/be/vitali/MainMenuController.java
@@ -1,17 +1,41 @@
 package be.vitali;
 
 import javafx.fxml.FXML;
-import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 public class MainMenuController {
-    @FXML private javafx.scene.control.Button closeButton;
+    //@FXML private javafx.scene.control.Button closeButton;
+    BorderPane borderPane;
+    HBox footer;
+    VBox menu;
+    Button recordButton;
+    Button settingsButton;
+    Button closeButton;
+    Button switchCamera;
 
-    @FXML
+    /*@FXML
     private void quit() {
         Stage stage = (Stage) closeButton.getScene().getWindow();
         stage.close();
-    }
+    }*/
 
     @FXML
-    public void initialize() { }
+    public void initialize() {
+        borderPane = new BorderPane();
+        footer = new HBox();
+        menu = new VBox();
+        recordButton = new Button();
+        settingsButton = new Button();
+        closeButton = new Button();
+        switchCamera = new Button();
+        footer.setStyle("-fx-background-color: #ff0000");
+        menu.setStyle("-fx-background-color: #00ff00");
+        footer.getChildren().addAll(closeButton, switchCamera);
+        menu.getChildren().addAll(recordButton, settingsButton);
+        borderPane.setCenter(menu);
+        borderPane.setBottom(footer);
+    }
 }
