cd2f6ae86405719af87dad899234511101d3c61d
vitalibeniamino <benvit@outlook.com>
2025-11-14 22:05:47 +0100

    Changed UI to be build more out of java

--- src/main/java/be/vitali/MainMenuController.java
+++ src/main/java/be/vitali/MainMenuController.java
@@ -4,11 +4,12 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 
 public class MainMenuController {
     //@FXML private javafx.scene.control.Button closeButton;
-    BorderPane borderPane;
+    @FXML private BorderPane borderPane;
     HBox footer;
     VBox menu;
     Button recordButton;
@@ -24,18 +25,32 @@ public class MainMenuController {
 
     @FXML
     public void initialize() {
-        borderPane = new BorderPane();
         footer = new HBox();
         menu = new VBox();
         recordButton = new Button();
         settingsButton = new Button();
         closeButton = new Button();
         switchCamera = new Button();
+
+        recordButton.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        settingsButton.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        closeButton.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        switchCamera.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+
+        HBox.setHgrow(closeButton, Priority.ALWAYS);
+        HBox.setHgrow(switchCamera, Priority.ALWAYS);
+        VBox.setVgrow(recordButton, Priority.ALWAYS);
+        VBox.setVgrow(settingsButton, Priority.ALWAYS);
+
         footer.setStyle("-fx-background-color: #ff0000");
         menu.setStyle("-fx-background-color: #00ff00");
+
         footer.getChildren().addAll(closeButton, switchCamera);
         menu.getChildren().addAll(recordButton, settingsButton);
+        
         borderPane.setCenter(menu);
         borderPane.setBottom(footer);
+        //borderPane.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        
     }
 }
